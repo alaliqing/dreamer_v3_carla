f0a0eec32eaaaf9c657fcc1762d58e50c9212091
Jing Qin <Qin@fzi.de>
2023-03-22 20:25:44 +0100

    complete missing code

--- dreamer.py
+++ dreamer.py
@@ -218,4 +218,165 @@ def make_env(config, logger, mode, train_eps, eval_eps):
     if (mode == "train") or (mode == "eval"):
         callbacks = [
             functools.partial(
-                process_episode, config, logger, mode, train_eps, ev
+                process_episode, config, logger, mode, train_eps, eval_eps
+            )
+        ]
+        env = wrappers.CollectDataset(env, callbacks)
+    env = wrappers.RewardObs(env)
+    return env
+
+
+def process_episode(config, logger, mode, train_eps, eval_eps, episode):
+    directory = dict(train=config.traindir, eval=config.evaldir)[mode]
+    cache = dict(train=train_eps, eval=eval_eps)[mode]
+    filename = tools.save_episodes(directory, [episode])[0]
+    length = len(episode["reward"]) - 1
+    score = float(episode["reward"].astype(np.float64).sum())
+    video = episode["image"]
+    # video = video[1000]
+    # print("video shape:", video.shape)
+    if mode == "eval":
+        cache.clear()
+    if mode == "train" and config.dataset_size:
+        total = 0
+        for key, ep in reversed(sorted(cache.items(), key=lambda x: x[0])):
+            if total <= config.dataset_size - length:
+                total += len(ep["reward"]) - 1
+            else:
+                del cache[key]
+        logger.scalar("dataset_size", total + length)
+    cache[str(filename)] = episode
+    print(f"{mode.title()} episode has {length} steps and return {score:.1f}.")
+    logger.scalar(f"{mode}_return", score)
+    logger.scalar(f"{mode}_length", length)
+    logger.scalar(f"{mode}_episodes", len(cache))
+    # logger.image(f"{mode}_image", video)
+    # if mode == "eval" or config.expl_gifs:
+        # logger.video(f"{mode}_policy", video[None])
+    logger.write()
+
+
+def main(config):
+    task = Task.init(project_name="bogdoll/rl_traffic_rule_Jing", task_name="google_dreamer_v3_carla", reuse_last_task_id=True)
+    # # task.set_base_docker(
+    # #             "nvcr.io/nvidia/pytorch:21.10-py3",
+    # #             docker_setup_bash_script="apt-get update && apt-get install -y python3-opencv",
+    # #             docker_arguments="-e NVIDIA_DRIVER_CAPABILITIES=all"  # --ipc=host",
+    # #             )
+    # task.set_base_docker(
+    #             "tks-zx-01.fzi.de/autonomous-agents/core-carla:21.10",
+    #             docker_setup_bash_script="apt-get update && apt-get install -y python3-opencv",
+    #             docker_arguments="-e NVIDIA_DRIVER_CAPABILITIES=all",  # --ipc=host",
+    #         )
+    # # task.add_requirements(
+    # #     package_name="setuptools",
+    # #     package_version="59.5.0",
+    # # )
+    # task.add_requirements(
+    #     package_name="moviepy",
+    #     package_version="1.0.3",
+    # )
+    # logger_clearml = task.get_logger()
+    # task.execute_remotely('tks-zx-01', clone=False, exit_process=True)
+
+    logdir = pathlib.Path(config.logdir).expanduser()
+    config.traindir = config.traindir or logdir / "train_eps"
+    config.evaldir = config.evaldir or logdir / "eval_eps"
+    config.steps //= config.action_repeat
+    config.eval_every //= config.action_repeat
+    config.log_every //= config.action_repeat
+    config.time_limit //= config.action_repeat
+    config.act = getattr(torch.nn, config.act)
+    config.norm = getattr(torch.nn, config.norm)
+
+    print("Logdir", logdir)
+    logdir.mkdir(parents=True, exist_ok=True)
+    config.traindir.mkdir(parents=True, exist_ok=True)
+    config.evaldir.mkdir(parents=True, exist_ok=True)
+    step = count_steps(config.traindir)
+    logger = tools.Logger(logdir, config.action_repeat * step)
+
+    print("Create envs.")
+    if config.offline_traindir:
+        directory = config.offline_traindir.format(**vars(config))
+    else:
+        directory = config.traindir
+    train_eps = tools.load_episodes(directory, limit=config.dataset_size)
+    if config.offline_evaldir:
+        directory = config.offline_evaldir.format(**vars(config))
+    else:
+        directory = config.evaldir
+    eval_eps = tools.load_episodes(directory, limit=1)
+    make = lambda mode: make_env(config, logger, mode, train_eps, eval_eps)
+    train_envs = [make("train") for _ in range(config.envs)]
+    eval_envs = [make("eval") for _ in range(config.envs)]
+    acts = train_envs[0].action_space
+    print("acts high and low", acts.high, acts.low)
+    config.num_actions = acts.n if hasattr(acts, "n") else acts.shape[0]
+
+    if not config.offline_traindir:
+        prefill = max(0, config.prefill - count_steps(config.traindir))
+        print(f"Prefill dataset ({prefill} steps).")
+        if hasattr(acts, "discrete"):
+            random_actor = tools.OneHotDist(
+                torch.zeros_like(torch.Tensor(acts.low))[None]
+            )
+        else:
+            random_actor = torchd.independent.Independent(
+                torchd.uniform.Uniform(
+                    torch.Tensor(acts.low)[None], torch.Tensor(acts.high)[None]
+                ),
+                1,
+            )
+
+        def random_agent(o, d, s, r):
+            action = random_actor.sample()
+            logprob = random_actor.log_prob(action)
+            return {"action": action, "logprob": logprob}, None
+
+        tools.simulate(random_agent, train_envs, prefill)
+        tools.simulate(random_agent, eval_envs, episodes=1)
+        logger.step = config.action_repeat * count_steps(config.traindir)
+
+    print("Simulate agent.")
+    train_dataset = make_dataset(train_eps, config)
+    eval_dataset = make_dataset(eval_eps, config)
+    agent = Dreamer(config, logger, train_dataset).to(config.device)
+    agent.requires_grad_(requires_grad=False)
+    if (logdir / "latest_model.pt").exists():
+        agent.load_state_dict(torch.load(logdir / "latest_model.pt"))
+        agent._should_pretrain._once = False
+
+    state = None
+    while agent._step < config.steps:
+        logger.write()
+        print("Start evaluation.")
+        video_pred = agent._wm.video_pred(next(eval_dataset))
+        logger.video("eval_openl", to_np(video_pred))
+        eval_policy = functools.partial(agent, training=False)
+        tools.simulate(eval_policy, eval_envs, episodes=1)
+        print("Start training.")
+        state = tools.simulate(agent, train_envs, config.eval_every, state=state)
+        torch.save(agent.state_dict(), logdir / "latest_model.pt")
+    for env in train_envs + eval_envs:
+        try:
+            env.close()
+        except Exception:
+            pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--configs", nargs="+", required=True)
+    args, remaining = parser.parse_known_args()
+    configs = yaml.safe_load(
+        (pathlib.Path(sys.argv[0]).parent / "configs.yaml").read_text()
+    )
+    defaults = {}
+    for name in args.configs:
+        defaults.update(configs[name])
+    parser = argparse.ArgumentParser()
+    for key, value in sorted(defaults.items(), key=lambda x: x[0]):
+        arg_type = tools.args_type(value)
+        parser.add_argument(f"--{key}", type=arg_type, default=arg_type(value))
+    main(parser.parse_args(remaining))
